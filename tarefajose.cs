94356f348834e55ecb9f5b9d5e357dc306b64ae7
YannPaim <161754380+YannPaim@users.noreply.github.com>
2024-03-06 19:24:50 -0300

    Create tarefajose.cs

---
+++ tarefajose.cs
@@ -0,0 +1,10 @@
+using System;
+class HelloWorld {
+  static void Main() {
+      
+    string MeuNome = "Guilherme Freitas Ferreira";
+    Console.WriteLine(MeuNome);
+    string NomeDoColega = "Yann Paim Ribeiro de Paula";
+    Console.WriteLine(NomeDoColega);
+  }
+}
